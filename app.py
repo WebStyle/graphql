7cb226d4f4bd1aa133ae018082bf9f0053efa74e
Farrukh <webstyle@protonmail.com>
2017-01-30 15:00:49 +0500

    added graphene

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import graphene
+
 print("Simple GraphQL with Python")
+
+class Query(graphene.ObjectType):
+    hello = graphene.String()
+
+    def resolve_hello(self, args, context, info):
+        return 'World'
+
+
+schema = graphene.Schema(query=Query)
+
+schema.execute('''
+  query {
+    hello
+  }
+''')
